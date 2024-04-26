32783e64c49f28feafa6742340ccb4dfb0ddef11
LBC <541014133@qq.com>
2024-04-26 19:00:48 +0800

    fix(): 解决 sql-slow-mirror 引起的 slf4j 依赖冲突

--- shing-generator-web-backend/src/main/java/com/shing/web/config/MyBatisPlusConfig.java
+++ shing-generator-web-backend/src/main/java/com/shing/web/config/MyBatisPlusConfig.java
@@ -3,9 +3,13 @@ package com.shing.web.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.cong.sql.slowmirror.config.ConfigUtils;
+import com.cong.sql.slowmirror.core.SqlAnalysisAspect;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
 
 /**
  * MyBatis Plus 配置
@@ -27,5 +31,26 @@ public class MyBatisPlusConfig {
         // 分页插件
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
+    }
+
+    /**
+     * SQL分析方面
+     *
+     * @return {@link SqlAnalysisAspect}
+     */
+    @Bean
+    public SqlAnalysisAspect sqlAnalysisAspect() {
+
+        // 加载配置文件，此处加载的是名为"sql.slow.mirror"的配置
+        Properties properties = ConfigUtils.loadConfig("sql.slow.mirror");
+
+        // 创建SQL分析切面的实例
+        SqlAnalysisAspect sqlAnalysisAspect = new SqlAnalysisAspect();
+
+        // 将加载的配置属性设置到SQL分析切面实例中
+        sqlAnalysisAspect.setProperties(properties);
+
+        // 返回配置完毕的SQL分析切面实例
+        return sqlAnalysisAspect;
     }
 }
